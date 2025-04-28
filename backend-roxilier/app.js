dbcbe332ac5a682420d9c1b3d220de0d879fabbb
Chetanshinde123 <shindechetan3408@gmail.com>
2025-04-28 10:35:05 +0530

    New done

--- backend-roxilier/app.js
+++ backend-roxilier/app.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // ✅ Allow only your frontend
-    credentials: true, // ✅ Allow sending cookies
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
 
